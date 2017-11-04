9446be1da8e9d1802e23b6ade1c49c34b32299a8
dravenxiaokai <jspankai@vip.qq.com>
2017-11-04 14:50:26 +0800

    状态 更新组件状态

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # draven-react
 react demo study
+
+browser-sync start --server --no-notify --files 'index.html,app/**/*.js'
--- app/main.js
+++ app/main.js
@@ -5,13 +5,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentBox from './comment/CommentBox'
 
-
-var comments = [
-    { "author": "draven", "date": "5 分钟前", "text": "天气不错啊！" },
-    { "author": "小凯", "date": "3 分钟前", "text": "出去玩啊！" }
-]
+// var comments = [
+//     { "author": "draven", "date": "5 分钟前", "text": "天气不错啊！" },
+//     { "author": "小凯", "date": "3 分钟前", "text": "出去玩啊！" }
+// ]
 
 ReactDOM.render(
-    <CommentBox data={comments} />,
+    <CommentBox url="app/comments.json" />,
     document.getElementById('app')
 )
