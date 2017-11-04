6a523efd5706ebb7d17c3163aa3021832cf6440b
dravenxiaokai <jspankai@vip.qq.com>
2017-11-04 15:17:09 +0800

    得到在浏览器上显示的数据 refs

--- app/comment/CommentForm.js
+++ app/comment/CommentForm.js
@@ -6,15 +6,20 @@ class CommentForm extends React.Component {
     handleSubmit(event) {
         event.preventDefault()
         console.log('提交表单')
+
+        let author = this.refs.author.value,
+            text = this.refs.text.value
+
+        console.log(author, text)
     }
     render() {
         return (
-            <form className="ui reply form" onSubmit={this.handleSubmit}>
+            <form className="ui reply form" onSubmit={this.handleSubmit.bind(this)}>
                 <div className="field">
-                    <input type="text" placeholder="姓名" />
+                    <input type="text" placeholder="姓名" ref="author" />
                 </div>
                 <div className="field">
-                    <textarea placeholder="评论"></textarea>
+                    <textarea placeholder="评论" ref="text"></textarea>
                 </div>
                 <button type="submit" className="ui blue button">添加评论</button>
             </form>
