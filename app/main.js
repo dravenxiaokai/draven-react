a4d8fbb5981ca5894a8bf4baa328bbc78c9be2a9
dravenxiaokai <jspankai@vip.qq.com>
2017-11-05 17:35:08 +0800

    react-router 还有问题

--- app/main.js
+++ app/main.js
@@ -3,14 +3,56 @@
 import 'semantic-ui/semantic.min.css!'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CommentBox from './comment/CommentBox'
+// import { Router, Route, Link } from 'react-router'
+import { BrowserRouter as Router, StaticRouter, Route, Link } from 'react-router-dom'
+// import createHistory from 'history/createBrowserHistory'
+// import createHashHistory from 'history/createHashHistory'
 
-// var comments = [
-//     { "author": "draven", "date": "5 分钟前", "text": "天气不错啊！" },
-//     { "author": "小凯", "date": "3 分钟前", "text": "出去玩啊！" }
-// ]
+// const history = createHistory()
 
-ReactDOM.render(
-    <CommentBox url="app/comments.json" />,
-    document.getElementById('app')
-)
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="ui secondary pointing menu">
+                    <Link to="/" className="item">首页</Link>
+                    <Link to="/tv" className="item">电视</Link>
+                </div>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+class TV extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="ui info message">电视节目列表</div>
+            </div>
+        )
+    }
+}
+
+class Show extends React.Component {
+    render() {
+        return (
+            <h3>节目</h3>
+        )
+    }
+}
+
+ReactDOM.render((
+    <Router>
+        <Route path="/" component={App}>
+            <Route path="tv" component={TV}>
+                <Route path="shows/:id" component={Show} />
+            </Route>
+        </Route>
+    </Router>
+), document.getElementById('app'))
+// ReactDOM.render((
+//     <BrowserRouter basename="/my">
+//         <App />
+//     </BrowserRouter>
+// ), document.getElementById('app'))
